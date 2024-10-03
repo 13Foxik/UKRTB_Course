62ae0228c6bcbeb508c8fb2af3d485522f8a49ee
Jugo2283 <kamgumer12@gmail.com>
2024-10-03 21:20:32 +0500

    Randomiziroval gifki

--- app/src/main/java/com/KKS/ukrtb_course/LoadingScreen.java
+++ app/src/main/java/com/KKS/ukrtb_course/LoadingScreen.java
@@ -6,7 +6,8 @@ import android.graphics.Gainmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
-
+import java.util.Random;
+import com.bumptech.glide.Glide;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,12 +15,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoadingScreen extends AppCompatActivity {
-
+    private int[] gifArray = {
+            R.drawable.birdgif,   //Массив гифок
+            R.drawable.beargif,
+            R.drawable.busycat,
+            R.drawable.pingvigif
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
-
+        ImageView imageview = findViewById(R.id.imageview); //пока не разобрался как работают нижние 7 строчек, впринципе похуй главное что работает
+        Random random = new Random();
+        int randomGifIndex = random.nextInt(gifArray.length);
+        Glide.with(this)
+                .asGif()
+                .load(gifArray[randomGifIndex])
+                .into(imageview);
         int freezeTime = 1000;
 
         Handler handler = new Handler();
