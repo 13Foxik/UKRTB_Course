4e4cea4103f2720a0bc0677d59f807ecbad499db
13Foxik <Romka.smeshnov@gmail.com>
2024-10-03 22:38:32 +0500

    Optimization gifs random

--- app/src/main/java/com/KKS/ukrtb_course/LoadingScreen.java
+++ app/src/main/java/com/KKS/ukrtb_course/LoadingScreen.java
@@ -1,38 +1,30 @@
 package com.KKS.ukrtb_course;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Gainmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 import java.util.Random;
 import com.bumptech.glide.Glide;
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoadingScreen extends AppCompatActivity {
-    private int[] gifArray = {
+    private final int freezeTime = 1000;
+
+    private final int[] gifArray = {
             R.drawable.birdgif,   //Массив гифок
             R.drawable.beargif,
             R.drawable.busycat,
             R.drawable.pingvigif
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
-        ImageView imageview = findViewById(R.id.imageview); //пока не разобрался как работают нижние 7 строчек, впринципе похуй главное что работает
-        Random random = new Random();
-        int randomGifIndex = random.nextInt(gifArray.length);
-        Glide.with(this)
-                .asGif()
-                .load(gifArray[randomGifIndex])
-                .into(imageview);
-        int freezeTime = 1000;
+
+        RandomizeGifs();
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -43,6 +35,16 @@ public class LoadingScreen extends AppCompatActivity {
                 LoadingScreen.this.finish();
             }
         }, freezeTime);
+    }
+    private void RandomizeGifs(){
+        Random random = new Random();
+        ImageView GifImage = findViewById(R.id.GifImage);
+        int randomGifIndex = random.nextInt(gifArray.length);
+
+        Glide.with(this)
+                .asGif()
+                .load(gifArray[randomGifIndex])
+                .into(GifImage);
     }
 
 }
