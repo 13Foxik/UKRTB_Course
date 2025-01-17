58df10194f1a8f7c48d6cf8478ae1cca7d830387
13Foxik <Romka.smeshnov@gmail.com>
2025-01-17 19:41:11 +0500

    admin menu 1.0

--- app/src/main/java/com/KKS/ukrtb_course/AdminMenu.java
+++ app/src/main/java/com/KKS/ukrtb_course/AdminMenu.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +22,16 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class AdminMenu extends AppCompatActivity {
+public class AdminMenu extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private ActivityAdminMenuBinding binding;
     private DatabaseReference mDataBase;
     private DatabaseReference uidRef;
     private String uid;
     private String emailText;
+    private Spinner spinner;
+    private String selectCourse;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,18 @@ public class AdminMenu extends AppCompatActivity {
         binding = ActivityAdminMenuBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_admin_menu);
         init();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId() == R.id.spinner){
+            selectCourse = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     private void init(){
@@ -48,9 +66,20 @@ public class AdminMenu extends AppCompatActivity {
             return;
         }
         uidRef = mDataBase.child("Users/").child(uid);
+        spinner = findViewById((R.id.spinner));
+
+        spinner.setOnItemSelectedListener(this);
+
+        String[] coursesName = getResources().getStringArray(R.array.nameCourses);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, coursesName);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
-    private void setAccessCourse(String nameCourse) {
+    private void setAccessCourse(String nameCourse, String UserID, Boolean accept) {
+        // Получаем ссылку на пользователя по UserID
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Users").child(UserID);
+
         ValueEventListener imageListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -60,15 +89,27 @@ public class AdminMenu extends AppCompatActivity {
                         user.accessOfCourses = new ArrayList<String>(); // Инициализация списка, если он null
                     }
 
-                    // Логика добавления или удаления "Unity"
-                    if (user.accessOfCourses.contains(nameCourse)) {
-                        user.accessOfCourses.remove(nameCourse);
-                    } else {
-                        user.accessOfCourses.add(nameCourse);
+                    // Логика добавления или удаления курса
+                    if (accept){
+                        if (user.accessOfCourses.contains(nameCourse)) {
+                            Toast.makeText(AdminMenu.this,
+                                    "Данному пользователю уже выдан доступ к " + nameCourse,
+                                    Toast.LENGTH_LONG).show();
+                        }else{
+                            user.accessOfCourses.add(nameCourse);
+                        }
+                    } else{
+                        if (!user.accessOfCourses.contains(nameCourse)) {
+                            Toast.makeText(AdminMenu.this,
+                                    "У данного пользователя уже ограничен доступ к " + nameCourse,
+                                    Toast.LENGTH_LONG).show();
+                        }else{
+                            user.accessOfCourses.remove(nameCourse);
+                        }
                     }
 
                     // Обновляем данные пользователя в Firebase
-                    uidRef.setValue(user).addOnCompleteListener(task -> {
+                    userRef.setValue(user).addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             Log.d("TAG", "User updated successfully");
                         } else {
@@ -87,61 +128,77 @@ public class AdminMenu extends AppCompatActivity {
             }
         };
 
-        Log.e("TAG", "Current User UID: " + uid);
-        uidRef.addListenerForSingleValueEvent(imageListener); // Используем addListenerForSingleValueEvent
+        Log.e("TAG", "Current User UID: " + UserID);
+        userRef.addListenerForSingleValueEvent(imageListener); // Используем addListenerForSingleValueEvent
     }
 
-    public void findUidByEmail(String email) {
-        Toast.makeText(AdminMenu.this,
-                email, Toast.LENGTH_SHORT).show();
+    // Интерфейс обратного вызова
+    public interface UidCallback {
+        void onCallback(String uid);
+    }
+
+    // Измененный метод findUidByEmail
+    public String findUidByEmail(String email ,UidCallback callback) {
         DatabaseReference database = FirebaseDatabase.getInstance().getReference("Users");
 
         database.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 boolean userFound = false;
+                String foundUid = null; // Переменная для хранения найденного UID
                 for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                     String userEmail = userSnapshot.child("email").getValue(String.class);
                     if (userEmail != null && userEmail.equals(email)) {
-                        String uid = userSnapshot.getKey(); // UID документа
-                        Toast.makeText(AdminMenu.this,
-                                "Uid пользователя: " + uid, Toast.LENGTH_SHORT).show();
+                        foundUid = userSnapshot.getKey(); // UID документа
                         userFound = true;
                         break; // Выход из цикла, если пользователь найден
                     }
                 }
-                if (!userFound) {
+                if (userFound) {
+                    callback.onCallback(foundUid); // Передаем найденный UID через callback
+                } else {
                     Log.d("Firebase", "Пользователь не найден");
+                    callback.onCallback(null); // Передаем null, если пользователь не найден
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.e("Firebase", "Ошибка получения UID", databaseError.toException());
+                callback.onCallback(null); // Передаем null в случае ошибки
             }
         });
-    }
-
-    public void OnClickFind(View view){
-        EditText emailEditText = findViewById(R.id.email);
-        emailText = emailEditText.getText().toString();
-        findUidByEmail(emailText);
+        return email;
     }
 
     public void OnClickBack(View view){
         startActivity(new Intent(AdminMenu.this, Profile.class));
         finish();
     }
-    public void OnClickUnity(View view){
-        setAccessCourse("Unity");
+
+    public void AcceptSettings(Boolean accept){
+        EditText emailEditText = findViewById(R.id.email);
+        String emailText = emailEditText.getText().toString(); // Исправлено на String
+        findUidByEmail(emailText, new UidCallback() {
+            @Override
+            public void onCallback(String uid) {
+                if (uid != null) {
+                    // UID найден, теперь можно вызывать setAccessCourse
+                    Log.d("Firebase", "Найденный UID: " + uid);
+                    setAccessCourse(selectCourse, uid, accept); // Передаем найденный UID в метод
+                } else {
+                    // UID не найден или произошла ошибка
+                    Log.d("Firebase", "UID не найден или произошла ошибка");
+                }
+            }
+        });
     }
-    public void OnClick1S(View view){
-        setAccessCourse("1S");
+
+    public void OnClickGiveAccept(View view) {
+        AcceptSettings(true);
     }
-    public void OnClickRobot(View view){
-        setAccessCourse("Robot");
+    public void OnClickRemoveAccept(View view) {
+        AcceptSettings(false);
     }
-    public void OnClickDron(View view){
-        setAccessCourse("Dron");
-    }
+
 }
